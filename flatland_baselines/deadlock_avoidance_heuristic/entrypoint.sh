65d3d1fb5ce096b52f9de9765ce9498e59483aa8
Christian Eichenberger <christianmarkus@oakmountain.ch>
2025-10-03 13:46:17 +0200

    Apply suggestions from code review

--- flatland_baselines/deadlock_avoidance_heuristic/entrypoint.sh
+++ flatland_baselines/deadlock_avoidance_heuristic/entrypoint.sh
@@ -7,7 +7,3 @@ export PYTHONPATH=$PWD
 
 flatland-trajectory-generate-from-policy --policy-pkg flatland_baselines.deadlock_avoidance_heuristic.policy.deadlock_avoidance_policy --policy-cls DeadLockAvoidancePolicy --obs-builder-pkg flatland_baselines.deadlock_avoidance_heuristic.observation.full_env_observation --obs-builder-cls FullEnvObservation $@
 
-
-
-
-
