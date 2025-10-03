1514bba8018f2f225dc74c62f0b3e825096f6434
chenkins <chenkins44@gmail.com>
2025-10-03 16:12:57 +0200

    chore(flatland_marl): import helpers from pytorch-tree-lstm repo.

---
+++ flatland_baselines/tree_lstm_ppo/pytorch_tree_lstm/example_usage.py
@@ -0,0 +1,47 @@
+# Source: https://github.com/michael-hahn/pytorch-tree-lstm/blob/master/example_usage.py
+import torch
+
+from .treelstm.util import calculate_evaluation_orders
+
+
+def _label_node_index(node, n=0):
+    node['index'] = n
+    for child in node['children']:
+        n += 1
+        _label_node_index(child, n)
+
+
+def _gather_node_attributes(node, key):
+    features = [node[key]]
+    for child in node['children']:
+        features.extend(_gather_node_attributes(child, key))
+    return features
+
+
+def _gather_adjacency_list(node):
+    adjacency_list = []
+    for child in node['children']:
+        adjacency_list.append([node['index'], child['index']])
+        adjacency_list.extend(_gather_adjacency_list(child))
+
+    return adjacency_list
+
+
+def convert_tree_to_tensors(tree, device=torch.device('cpu')):
+    # Label each node with its walk order to match nodes to feature tensor indexes
+    # This modifies the original tree as a side effect
+    _label_node_index(tree)
+
+    features = _gather_node_attributes(tree, 'features')
+    labels = _gather_node_attributes(tree, 'labels')
+    adjacency_list = _gather_adjacency_list(tree)
+
+    node_order, edge_order = calculate_evaluation_orders(adjacency_list, len(features))
+
+    return {
+        'features': torch.tensor(features, device=device, dtype=torch.float32),
+        'labels': torch.tensor(labels, device=device, dtype=torch.float32),
+        'node_order': torch.tensor(node_order, device=device, dtype=torch.int64),
+        'adjacency_list': torch.tensor(adjacency_list, device=device, dtype=torch.int64),
+        'edge_order': torch.tensor(edge_order, device=device, dtype=torch.int64),
+    }
