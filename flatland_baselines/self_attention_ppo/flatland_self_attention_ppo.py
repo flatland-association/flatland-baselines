18e621dcd64c3526cf969982bb2b30060c4ab703
chenkins <chenkins44@gmail.com>
2025-10-15 19:31:36 +0200

    feat(baselines): reduce tree depth to keep num agents. Add Flatland metrics.

--- flatland_baselines/self_attention_ppo/flatland_self_attention_ppo.py
+++ flatland_baselines/self_attention_ppo/flatland_self_attention_ppo.py
@@ -28,6 +28,7 @@ from flatland.core.env_observation_builder import AgentHandle
 from flatland.envs.observations import TreeObsForRailEnv, Node
 from flatland.envs.predictions import ShortestPathPredictorForRailEnv
 from flatland.envs.rail_env import RailEnv
+from flatland.ml.observations.flatten_tree_observation_for_rail_env import FlattenedNormalizedTreeObsForRailEnv
 from flatland.ml.observations.gym_observation_builder import GymObservationBuilder
 from flatland.ml.ray.examples.flatland_observation_builders_registry import register_flatland_ray_cli_observation_builders
 from flatland.ml.ray.wrappers import ray_env_generator
@@ -156,6 +157,71 @@ def make_complete_nary(node: Node, depth, max_depth, tree_explored_actions_char)
             if a not in node.childs or node.childs[a] == -np.inf:
                 node.childs[a] = Node(*[-np.inf] * 12, {})
             make_complete_nary(node.childs[a], depth + 1, max_depth, tree_explored_actions_char)
+
+
+import gymnasium as gym
+from ray.rllib.callbacks.callbacks import RLlibCallback
+from ray.rllib.env.multi_agent_episode import MultiAgentEpisode
+from ray.rllib.env.vector.vector_multi_agent_env import VectorMultiAgentEnv
+
+from flatland.envs.rail_env import RailEnv
+from flatland.ml.ray.ray_multi_agent_rail_env import RayMultiAgentWrapper
+
+
+# TODO backport to flatland-rl
+# See also for https://gitlab.aicrowd.com/flatland/neurips2020-flatland-baselines/-/blob/master/train.py?ref_type=heads
+class FlatlandMetricsCallback(RLlibCallback):
+    """
+    Add `normalized_reward` and `percentage_complete` evaluation metrics.
+    """
+
+    def on_episode_end(
+            self,
+            *,
+            episode: MultiAgentEpisode,
+            env_runner,
+            metrics_logger,
+            env,
+            env_index,
+            rl_module,
+            **kwargs,
+    ) -> None:
+        # If we have a vector env, only render the sub-env at index 0.
+        if isinstance(env.unwrapped, (gym.vector.VectorEnv, VectorMultiAgentEnv)):
+            unwrapped = env.unwrapped.envs[0]
+        else:
+            unwrapped = env.unwrapped
+        while not isinstance(unwrapped, RayMultiAgentWrapper):
+            unwrapped = unwrapped.unwrapped
+        rail_env: RailEnv = unwrapped._wrap
+
+        rewards_dict = episode.get_rewards(-1)
+        episode.get_state()
+        episode_done_agents = 0
+        for h in rail_env.get_agent_handles():
+            if rail_env.dones[h]:
+                episode_done_agents += 1
+
+        episode_num_agents = len(rewards_dict)
+        assert episode_num_agents == rail_env.get_num_agents()
+        assert sum(list(rewards_dict.values())) == sum(list(rail_env.rewards_dict.values()))
+        # https://flatland-association.github.io/flatland-book/challenges/flatland3/eval.html
+        normalized_reward = sum(list(rewards_dict.values())) / (
+                rail_env._max_episode_steps *
+                episode_num_agents
+        )
+
+        metrics_logger.log_value(
+            "normalized_reward",
+            normalized_reward,
+            reduce="sum",
+        )
+
+        percentage_complete = float(episode_done_agents) / episode_num_agents
+        metrics_logger.log_value(
+            "percentage_complete",
+            percentage_complete,
+        )
 
 
 # https://docs.ray.io/en/latest/rllib/getting-started.html#rllib-python-api
@@ -341,12 +407,13 @@ def train(args: Optional[argparse.Namespace] = None, init_args=None) -> Union[Re
                     module_class=SelfAttentionTorchRLModule,
                     model_config={
                         "hidden_sz": 128,
-                        "tree_embedding_sz": 1020,
+                        "tree_embedding_sz": registry_get_input(args.obs_builder)().get_observation_space().shape[0],
                         "action_sz": 5
                     }
                 )},
             )
         )
+        .callbacks(FlatlandMetricsCallback)
     )
     res = run_rllib_example_script_experiment(base_config, args)
 
@@ -360,11 +427,13 @@ if __name__ == '__main__':
 
     register_input("UngroupedFlattenedTreeObsForRailEnv_max_depth_3_50",
                    lambda: UngroupedFlattenedTreeObsForRailEnv(max_depth=3, predictor=ShortestPathPredictorForRailEnv(max_depth=50)))
+    register_input("FlattenedNormalizedTreeObsForRailEnv_max_depth_2_50",
+                   lambda: FlattenedNormalizedTreeObsForRailEnv(max_depth=2, predictor=ShortestPathPredictorForRailEnv(max_depth=50)))
     parser = add_flatland_training_with_parameter_sharing_args()
 
     train(parser.parse_args([
         "--num-agents", "50",
-        "--obs-builder", "FlattenedNormalizedTreeObsForRailEnv_max_depth_3_50",
+        "--obs-builder", "FlattenedNormalizedTreeObsForRailEnv_max_depth_2_50",
         "--algo", "PPO",
         "--evaluation-num-env-runners", "2", "--evaluation-interval", "1",
         "--checkpoint-freq", "1",
