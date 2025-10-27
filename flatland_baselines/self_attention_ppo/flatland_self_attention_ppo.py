9903b4e7b0209a25db75d4c7c21060a8c71fbb39
chenkins <chenkins44@gmail.com>
2025-10-27 15:15:51 +0100

    refactor(ml-RLlib): separate cli from training loop.

--- flatland_baselines/self_attention_ppo/flatland_self_attention_ppo.py
+++ flatland_baselines/self_attention_ppo/flatland_self_attention_ppo.py
@@ -2,42 +2,25 @@
 Trains custom torch model on Flatland env in RLlib using single policy learning.
 Based on https://github.com/ray-project/ray/blob/master/rllib/examples/multi_agent/pettingzoo_parameter_sharing.py.
 """
-import argparse
-import logging
-from typing import Union, Optional
+from typing import Optional
 
 import gymnasium as gym
 import numpy as np
-import ray
 import torch.nn as nn
-from ray import tune
-from ray.rllib.algorithms import AlgorithmConfig
 from ray.rllib.core import Columns
 from ray.rllib.core.rl_module.apis import ValueFunctionAPI
-from ray.rllib.core.rl_module.multi_rl_module import MultiRLModuleSpec
-from ray.rllib.core.rl_module.rl_module import RLModuleSpec
 from ray.rllib.core.rl_module.torch import TorchRLModule
 from ray.rllib.utils import override
-from ray.rllib.utils.test_utils import (
-    add_rllib_example_script_args,
-    run_rllib_example_script_experiment,
-)
-from ray.rllib.utils.typing import ResultDict
-from ray.tune.registry import get_trainable_cls, register_env, registry_get_input, register_input
+from ray.rllib.utils.test_utils import add_rllib_example_script_args
+from ray.tune.registry import registry_get_input, register_input
 
 from flatland.core.env_observation_builder import AgentHandle
 from flatland.envs.observations import TreeObsForRailEnv, Node
-from flatland.envs.predictions import ShortestPathPredictorForRailEnv
 from flatland.envs.rail_env import RailEnv
-from flatland.ml.observations.flatten_tree_observation_for_rail_env import FlattenedNormalizedTreeObsForRailEnv
 from flatland.ml.observations.gym_observation_builder import GymObservationBuilder
 from flatland.ml.ray.examples.flatland_observation_builders_registry import register_flatland_ray_cli_observation_builders
-from flatland.ml.ray.wrappers import ray_env_generator
+from flatland.ml.ray.examples.flatland_training_with_parameter_sharing import train_with_parameter_sharing
 from flatland_baselines.tree_lstm_ppo.flatland_marl.net_tree import Transformer
-
-from flatland.ml.ray.FlatlandMetricsAndTrajectoryCallback import FlatlandMetricsAndTrajectoryCallback
-from flatland.ml.ray.FlatlandMetricsCallback import FlatlandMetricsCallback
-
 
 
 # TODO backport to flatland-rl
@@ -164,9 +147,6 @@ def make_complete_nary(node: Node, depth, max_depth, tree_explored_actions_char)
             make_complete_nary(node.childs[a], depth + 1, max_depth, tree_explored_actions_char)
 
 
-import gymnasium as gym
-
-
 # https://docs.ray.io/en/latest/rllib/getting-started.html#rllib-python-api
 # TODO make configurable/registry?
 class SelfAttentionTorchRLModule(TorchRLModule, ValueFunctionAPI):
@@ -234,164 +214,46 @@ class SelfAttentionTorchRLModule(TorchRLModule, ValueFunctionAPI):
         return self.critic_net.forward(embedding).squeeze(-1).squeeze(0)
 
 
-def setup_func():
-    logging.basicConfig(level=logging.INFO, format="[%(asctime)s][%(levelname)s][%(process)d][%(filename)s:%(funcName)s:%(lineno)d] %(message)s")
-
-
-def add_flatland_training_with_parameter_sharing_args():
-    parser = add_rllib_example_script_args(
-        default_iters=200,
-        default_timesteps=1000000,
-        default_reward=0.0,
-    )
-    parser.set_defaults(
-        enable_new_api_stack=True
-    )
-    parser.add_argument(
-        "--train-batch-size-per-learner",
-        type=int,
-        default=4000,
-        help="See https://docs.ray.io/en/latest/rllib/package_ref/doc/ray.rllib.algorithms.algorithm_config.AlgorithmConfig.training.html#ray.rllib.algorithms.algorithm_config.AlgorithmConfig.training",
-    )
-    parser.add_argument(
-        "--obs-builder",
-        type=str,
-        default=None,
-    )
-    parser.add_argument(
-        "--ray-address",
-        type=str,
-        default=None,
-        required=False,
-        help="The address of the ray cluster to connect to in the form ray://<head_node_ip_address>:10001. Leave empty to start a new cluster. Passed to ray.init(address=...). See https://docs.ray.io/en/latest/ray-core/api/doc/ray.init.html ",
-    )
-    parser.add_argument("--env_var", "-e",
-                        metavar="KEY=VALUE",
-                        nargs='*',
-                        help="Set ray runtime environment variables like -e RAY_DEBUG=1, passed to ray.init(runtime_env={env_vars: {...}}), see https://docs.ray.io/en/latest/ray-core/handling-dependencies.html#api-reference")
-    return parser
-
-
-def train(args: Optional[argparse.Namespace] = None, init_args=None) -> Union[ResultDict, tune.result_grid.ResultGrid]:
-    if args is None:
-        parser = add_flatland_training_with_parameter_sharing_args()
-        args = parser.parse_args()
-    assert args.num_agents > 0, "Must set --num-agents > 0 when running this script!"
-    assert (
-        args.enable_new_api_stack
-    ), "Must set --enable-new-api-stack when running this script!"
-    assert (
-        args.obs_builder
-    ), "Must set --obs-builder <obs builder ID> when running this script!"
-
-    setup_func()
-    if init_args is None:
-        env_vars = set()
-        if args.env_var is not None:
-            env_vars = args.env_var
-        init_args = {
-            # https://docs.ray.io/en/latest/ray-core/handling-dependencies.html#runtime-environments
-            "runtime_env": {
-                "env_vars": dict(map(lambda s: s.split('='), env_vars)),
-                # https://docs.ray.io/en/latest/ray-observability/user-guides/configure-logging.html
-                "worker_process_setup_hook": "flatland.ml.ray.examples.flatland_training_with_parameter_sharing.setup_func"
-            },
-            "ignore_reinit_error": True,
-        }
-        if args.ray_address is not None:
-            init_args['address'] = args.ray_address
-
-    # https://docs.ray.io/en/latest/ray-core/api/doc/ray.init.html
-    ray.init(
-        **init_args,
-    )
-    env_name = "flatland_env"
-    # test_id,env_id,n_agents,x_dim,y_dim,n_cities,max_rail_pairs_in_city,n_envs_run,grid_mode,max_rails_between_cities,malfunction_duration_min,malfunction_duration_max,malfunction_interval,speed_ratios,random_seed
-    # Test_3,Level_0,50,30,35,3,2,10,False,2,20,50,4500,"{1.0: 0.25, 0.5: 0.25, 0.33: 0.25, 0.25: 0.25}",8524649404651236810
-    # x_dim=30,
-    register_env(env_name, lambda _: ray_env_generator(
-        n_agents=args.num_agents,
-        obs_builder_object=registry_get_input(args.obs_builder)(),
-        x_dim=30,
-        y_dim=35,
-        n_cities=3,
-        max_rail_pairs_in_city=2,
-        grid_mode=False,
-        max_rails_between_cities=2,
-        malfunction_duration_min=20,
-        malfunction_duration_max=50,
-        malfunction_interval=540,
-        speed_ratios={1.0: 0.25, 0.5: 0.25, 0.33: 0.25, 0.25: 0.25},
-        seed=int(np.random.default_rng().integers(2 ** 32 - 1)),
-    ))
-
-    # TODO could be extracted to cli - keep it low key as illustration only
-    additional_training_config = {}
-    if args.algo == "DQN":
-        additional_training_config = {"replay_buffer_config": {
-            "type": "MultiAgentEpisodeReplayBuffer",
-        }}
-    base_config = (
-        # N.B. the warning `passive_env_checker.py:164: UserWarning: WARN: The obs returned by the `reset()` method was expecting numpy array dtype to be float32, actual type: float64`
-        #   comes from ray.tune.registry._register_all() -->  import ray.rllib.algorithms.dreamerv3 as dreamerv3!
-        get_trainable_cls(args.algo)
-        .get_default_config()
-        .environment("flatland_env")
-        .multi_agent(
-            policies={"p0"},
-            # All agents map to the exact same policy.
-            policy_mapping_fn=(lambda aid, *args, **kwargs: "p0"),
-        )
-        .training(
-            model={
-                # "vf_share_layers": True,
-            },
-            train_batch_size=args.train_batch_size_per_learner,
-            **additional_training_config
-        )
-        .rl_module(
-            rl_module_spec=MultiRLModuleSpec(
-                rl_module_specs={"p0": RLModuleSpec(
-                    module_class=SelfAttentionTorchRLModule,
-                    model_config={
-                        "hidden_sz": 128,
-                        "tree_embedding_sz": registry_get_input(args.obs_builder)().get_observation_space().shape[0],
-                        "action_sz": 5
-                    }
-                )},
-            )
-        )
-        .callbacks(FlatlandMetricsCallback)
-        .evaluation(
-            # TODO inject from cli
-            evaluation_config=AlgorithmConfig.overrides(callbacks=FlatlandMetricsAndTrajectoryCallback),
-        )
-    )
-    res = run_rllib_example_script_experiment(base_config, args)
-
-    if res.num_errors > 0:
-        raise AssertionError(f"{res.errors}")
-    return res
-
-
 if __name__ == '__main__':
+    # TODO should not be necessary
     register_flatland_ray_cli_observation_builders()
+    registered_obs_builder = "FlattenedNormalizedTreeObsForRailEnv_max_depth_2_50"
+    register_input("SelfAttentionTorchRLModule", SelfAttentionTorchRLModule)
 
-    register_input("UngroupedFlattenedTreeObsForRailEnv_max_depth_3_50",
-                   lambda: UngroupedFlattenedTreeObsForRailEnv(max_depth=3, predictor=ShortestPathPredictorForRailEnv(max_depth=50)))
-    register_input("FlattenedNormalizedTreeObsForRailEnv_max_depth_2_50",
-                   lambda: FlattenedNormalizedTreeObsForRailEnv(max_depth=2, predictor=ShortestPathPredictorForRailEnv(max_depth=50)))
-    parser = add_flatland_training_with_parameter_sharing_args()
+    parser = add_rllib_example_script_args()
+    train_with_parameter_sharing(
+        # args going into run_rllib_example_script_experiment()
+        args=parser.parse_args([
+            "--enable-new-api-stack",
+            "--algo", "PPO",
+            "--evaluation-interval", "1",
+            "--checkpoint-freq", "1",
+            "--num-agents", "50",
+        ]),
 
-    train(parser.parse_args([
-        "--num-agents", "50",
-        "--obs-builder", "FlattenedNormalizedTreeObsForRailEnv_max_depth_2_50",
-        "--algo", "PPO",
-        "--evaluation-interval", "1",
-        "--checkpoint-freq", "1",
-        "--train-batch-size-per-learner", "500",
-    ]))
-# TODO custom eval with small, middle and large envs
-# TODO wandb logging
-# TODO generate movies during eval
-# TODO generate stats nb during eval
+        train_batch_size_per_learner=500,
+        module_class="SelfAttentionTorchRLModule",
+        obs_builder=registered_obs_builder,
+        model_config={
+            "hidden_sz": 128,
+            "tree_embedding_sz": registry_get_input(registered_obs_builder)().get_observation_space().shape[0],
+            "action_sz": 5
+        },
+        # test_id,env_id,n_agents,x_dim,y_dim,n_cities,max_rail_pairs_in_city,n_envs_run,seed,grid_mode,max_rails_between_cities,malfunction_duration_min,malfunction_duration_max,malfunction_interval,speed_ratios
+        # Test_03,Level_0,50,30,35,3,2,10,42,False,2,20,50,540,"{1.0: 0.25, 0.5: 0.25, 0.33: 0.25, 0.25: 0.25}"
+        # see https://flatland-association.github.io/flatland-book/challenges/flatland3/envconfig.html Round 2, Test03
+        env_config=dict(
+            x_dim=30,
+            y_dim=35,
+            n_cities=3,
+            max_rail_pairs_in_city=2,
+            grid_mode=False,
+            max_rails_between_cities=2,
+            malfunction_duration_min=20,
+            malfunction_duration_max=50,
+            malfunction_interval=540,
+            speed_ratios={1.0: 0.25, 0.5: 0.25, 0.33: 0.25, 0.25: 0.25}, ),
+    )
+
+    # TODO custom eval with small, middle and large envs
+    # TODO test wandb logging
