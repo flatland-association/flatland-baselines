3161141a46425189a6a656da3da07ea4bc18a3c4
chenkins <chenkins44@gmail.com>
2026-01-21 18:57:17 +0100

    feat: add mean (episode) reward.

--- flatland_baselines/deadlock_avoidance_heuristic/offline_demo/offline.py
+++ flatland_baselines/deadlock_avoidance_heuristic/offline_demo/offline.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 
+import numpy as np
+import pytest
+from flatland.trajectories.policy_grid_runner import generate_trajectories_from_metadata
+
 from flatland.evaluators.trajectory_analysis import data_frame_for_trajectories
 
 if __name__ == '__main__':
     data_dir = Path("./results_environments_v2").resolve()
-    if False:
-        metadata_csv = Path("/Users/che/workspaces/flatland-scenarios/scenarios/metadata.csv").resolve()
+    if True:
+        metadata_csv = Path("/Users/che/workspaces/flatland-scenarios/trajectories/malfunction_deadlock_avoidance_heuristics/metadata.csv").resolve()
 
         data_dir.mkdir()
         with pytest.raises(SystemExit) as e_info:
@@ -23,8 +27,26 @@ if __name__ == '__main__':
         all_actions, all_trains_positions, all_trains_arrived, all_trains_rewards_dones_infos, env_stats, agent_stats = data_frame_for_trajectories(
             root_data_dir=Path(data_dir))
         print(all_trains_arrived)
-        print(all_trains_arrived["mean_normalized_reward"].sum())
-        print(all_trains_arrived["mean_normalized_reward"].mean())
-        print(all_trains_arrived["success_rate"].mean())
-        # TODO take mean of summs grouped by test/level
-        print(all_trains_rewards_dones_infos["reward"].sum())
+
+        sum_normalized_reward = all_trains_arrived["mean_normalized_reward"].sum()
+        mean_normalized_reward = all_trains_arrived["mean_normalized_reward"].mean()
+        mean_percentage_complete = all_trains_arrived["success_rate"].mean()
+        mean_reward = all_trains_rewards_dones_infos.groupby(['episode_id']).agg({"reward": "sum"}).mean()['reward']
+
+        # Round off the reward values, see service.py
+        mean_reward = round(mean_reward, 2)
+        mean_normalized_reward = round(mean_normalized_reward, 5)
+        mean_percentage_complete = round(mean_percentage_complete, 3)
+
+        print(f"# Mean Reward : {mean_reward}")
+        print(f"# Sum Normalized Reward : {sum_normalized_reward} (primary score)")
+        print(f"# Mean Percentage Complete : {mean_percentage_complete} (secondary score)")
+        print(f"# Mean Normalized Reward : {mean_normalized_reward}")
+
+        # TODO rounding problem?
+        assert mean_reward == -3541.52
+        assert np.isclose(43.08898598301832, sum_normalized_reward)
+        assert mean_percentage_complete == 0.678
+        assert mean_normalized_reward == 0.86178
+
+
