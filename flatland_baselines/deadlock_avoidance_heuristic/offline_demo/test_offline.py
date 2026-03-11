79cfd92322be7b43349fafe420ea8bc3703918e4
chenkins <chenkins44@gmail.com>
2026-03-11 17:22:51 +0100

    refactor: extract method.
    Signed-off-by: chenkins <chenkins44@gmail.com>

--- flatland_baselines/deadlock_avoidance_heuristic/offline_demo/test_offline.py
+++ flatland_baselines/deadlock_avoidance_heuristic/offline_demo/test_offline.py
@@ -34,31 +34,35 @@ def test_offline_calibrated_against_offline_legacy_way():
                 "--post-seed", 1001,
             ])
         assert e_info.value.code == 0
-        all_actions, all_trains_positions, all_trains_arrived, all_trains_rewards_dones_infos, env_stats, agent_stats = data_frame_for_trajectories(
-            root_data_dir=Path(data_dir))
-        print(all_trains_arrived)
+        verify_online_offline_calibration(data_dir)
 
-        sum_normalized_reward = all_trains_arrived["normalized_reward"].sum()
-        mean_normalized_reward = all_trains_arrived["normalized_reward"].mean()
-        mean_percentage_complete = all_trains_arrived["success_rate"].mean()
-        mean_reward = all_trains_rewards_dones_infos.groupby(['episode_id']).agg({"reward": "sum"}).mean()['reward']
 
-        # Round off the reward values, see service.py
-        mean_reward = round(mean_reward, 2)
-        mean_normalized_reward = round(mean_normalized_reward, 5)
-        mean_percentage_complete = round(mean_percentage_complete, 3)
+def verify_online_offline_calibration(data_dir: Path):
+    all_actions, all_trains_positions, all_trains_arrived, all_trains_rewards_dones_infos, env_stats, agent_stats = data_frame_for_trajectories(
+        root_data_dir=Path(data_dir))
+    print(all_trains_arrived)
 
-        print(f"# Mean Reward : {mean_reward}")
-        print(f"# Sum Normalized Reward : {sum_normalized_reward} (primary score)")
-        print(f"# Mean Percentage Complete : {mean_percentage_complete} (secondary score)")
-        print(f"# Mean Normalized Reward : {mean_normalized_reward}")
+    sum_normalized_reward = all_trains_arrived["normalized_reward"].sum()
+    mean_normalized_reward = all_trains_arrived["normalized_reward"].mean()
+    mean_percentage_complete = all_trains_arrived["success_rate"].mean()
+    mean_reward = all_trains_rewards_dones_infos.groupby(['episode_id']).agg({"reward": "sum"}).mean()['reward']
 
-        # From online evaluation, see
-        # evaluator-1   | # Mean Reward : -3427.52
-        # evaluator-1   | # Sum Normalized Reward : 43.141500749639434 (primary score)
-        # evaluator-1   | # Mean Percentage Complete : 0.671 (secondary score)
-        # evaluator-1   | # Mean Normalized Reward : 0.86283
-        assert np.isclose(mean_reward, -3427.52)
-        assert np.isclose(sum_normalized_reward, 43.141500749639434)
-        assert np.isclose(mean_percentage_complete, 0.671)
-        assert np.isclose(mean_normalized_reward, 0.86283)
+    # Round off the reward values, see service.py
+    mean_reward = round(mean_reward, 2)
+    mean_normalized_reward = round(mean_normalized_reward, 5)
+    mean_percentage_complete = round(mean_percentage_complete, 3)
+
+    print(f"# Mean Reward : {mean_reward}")
+    print(f"# Sum Normalized Reward : {sum_normalized_reward} (primary score)")
+    print(f"# Mean Percentage Complete : {mean_percentage_complete} (secondary score)")
+    print(f"# Mean Normalized Reward : {mean_normalized_reward}")
+
+    # From online evaluation, see
+    # evaluator-1   | # Mean Reward : -3427.52
+    # evaluator-1   | # Sum Normalized Reward : 43.141500749639434 (primary score)
+    # evaluator-1   | # Mean Percentage Complete : 0.671 (secondary score)
+    # evaluator-1   | # Mean Normalized Reward : 0.86283
+    assert np.isclose(mean_reward, -3427.52)
+    assert np.isclose(sum_normalized_reward, 43.141500749639434)
+    assert np.isclose(mean_percentage_complete, 0.671)
+    assert np.isclose(mean_normalized_reward, 0.86283)
